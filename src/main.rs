850f112e1903f4d026148f26d44cdeeebb4a2036
Alastair Douglas <altreus@altre.us>
2023-12-07 18:24:06 +0000

    Part 2: words as well

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+use std::env;
 use std::fs::read_to_string;
 use regex::Regex;
 
+const NUMS : [&str; 9] = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+const RENUMS : &str = "(one|two|three|four|five|six|seven|eight|nine|\\d)";
+
 fn main() {
-    let total = read_to_string("input.txt")
+    let argv : Vec<_> = env::args().collect();
+    let total = read_to_string(&argv[1])
         .unwrap()
         .lines()
         .map(|l| find_calibration_values(l))
@@ -11,23 +16,32 @@ fn main() {
     println!("{}", total);
 }
 
+fn str_to_i64(string: &str) -> i64 {
+    if let Some(idx) = NUMS.iter().position(|x| x == &string) {
+        return (idx + 1) as i64;
+    }
+
+    return string.parse::<i64>().unwrap();
+}
+
 fn find_calibration_values(line: &str) -> i64 {
-    let re_1d = Regex::new(r"(\d)").unwrap();
-    let re_2d = Regex::new(r"(\d).*(\d)").unwrap();
+    let re_num1 = Regex::new(RENUMS).unwrap();
+    let re_num2 = Regex::new(format!(".*{}", RENUMS).as_str()).unwrap();
 
-    if let Some(res) = re_2d.captures(line) {
-        let ( _, [num1, num2] ) = res.extract();
+    let (digit1, digit2) : (i64, i64);
 
+    let res = re_num1.captures(line).unwrap();
+    let ( _, [num1] ) = res.extract();
 
-        let cal = num1.to_owned() + num2;
-        return cal.parse::<i64>().unwrap();
-    }
+    digit1 = str_to_i64(num1);
 
-    if let Some(res) = re_1d.captures(line) {
+    if let Some(res) = re_num2.captures(line) {
         let (_, [num]) = res.extract();
-        let cal = num.to_owned() + num;
-        return cal.parse::<i64>().unwrap();
+        digit2 = str_to_i64(num);
+    }
+    else {
+        digit2 = digit1;
     }
 
-    return 0;
+    return digit1 * 10 + digit2;
 }
