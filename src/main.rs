274f8ea2f1a909f02a87f61873c32fa996986f83
Alastair Douglas <altreus@altre.us>
2023-12-07 12:55:55 +0000

    Refactor in terms of map

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,32 @@ use std::fs::read_to_string;
 use regex::Regex;
 
 fn main() {
-    let mut total : i64 = 0;
-
-    let re_1d = Regex::new(r"(\d)").unwrap();
-    let re_2d = Regex::new(r"(\d).*(\d)").unwrap();
-    for line in read_to_string("input.txt").unwrap().lines() {
-        let cap = re_2d.captures(line);
-        match cap {
-            None => {
-                let cap2 = re_1d.captures(line);
-                match cap2 {
-                    None => { continue; }
-                    Some(res) => {
-                        let (_, [num]) = res.extract();
-                        let cal = num.to_owned() + num;
-                        total += cal.parse::<i64>().unwrap();
-                    }
-                }
-            }
-            Some(res) => {
-                let ( _, [num1, num2] ) = res.extract();
-
-
-                let cal = num1.to_owned() + num2;
-                total += cal.parse::<i64>().unwrap();
-            }
-        }
-    }
+    let total = read_to_string("input.txt")
+        .unwrap()
+        .lines()
+        .map(|l| find_calibration_values(l))
+        .sum::<i64>();
 
     println!("{}", total);
+}
+
+fn find_calibration_values(line: &str) -> i64 {
+    let re_1d = Regex::new(r"(\d)").unwrap();
+    let re_2d = Regex::new(r"(\d).*(\d)").unwrap();
+
+    if let Some(res) = re_2d.captures(line) {
+        let ( _, [num1, num2] ) = res.extract();
+
+
+        let cal = num1.to_owned() + num2;
+        return cal.parse::<i64>().unwrap();
+    }
+
+    if let Some(res) = re_1d.captures(line) {
+        let (_, [num]) = res.extract();
+        let cal = num.to_owned() + num;
+        return cal.parse::<i64>().unwrap();
+    }
+
+    return 0;
 }
